2eea5c3228bd5324061a1ebe304632ca949b1e14
Dhairya Parikh <dhairyaparikh769@gmail.com>
2024-09-27 00:30:09 +0530

    Favicon and Heading Updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import Container from "./ui/Container";
 import MainNav from "./MainNav";
 import getCategories from "@/actions/getCategories";
 import NavbarActions from "./NavbarActions";
+import { ShoppingBasket } from "lucide-react";
 
 
 
@@ -15,7 +16,8 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">STORE</p>
+          <ShoppingBasket/>
+            <p className="font-bold text-xl">NEXT SHOP</p>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
