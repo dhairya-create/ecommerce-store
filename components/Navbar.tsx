77612e1d3439b513674032222060a2f6d1fa9636
Dhairya Parikh <dhairyaparikh769@gmail.com>
2024-09-27 09:07:23 +0530

    Navbar Updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,8 +16,8 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-          <ShoppingBasket/>
-            <p className="font-bold text-xl">NEXT SHOP</p>
+          <ShoppingBasket size={20}/>
+            <p className="font-bold text-sm md:text-xl whitespace-nowrap">SHOP</p>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
